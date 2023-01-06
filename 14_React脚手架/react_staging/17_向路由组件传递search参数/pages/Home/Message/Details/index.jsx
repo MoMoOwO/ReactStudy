88080826e4d62b97ff0e194395419363b676cb82
MoMoOwO <strangersj@outlook.com>
2023-01-06 15:41:52 +0800

    向路由组件传递 search 参数

---
+++ 14_React脚手架/react_staging/17_向路由组件传递search参数/pages/Home/Message/Details/index.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import qs from 'querystringify'
+
+const messageDetails = [
+	{ id: '01', content: '你好，中国' },
+	{ id: '02', content: '你好，山东' },
+	{ id: '03', content: '你好，济南' }
+]
+
+export default class Details extends Component {
+	render() {
+		console.log(this.props)
+		// 接受 params 参数（url 进行传参）
+		/* const { id, title } = this.props.match.params */
+
+		// 接受 search 参数
+		const { search } = this.props.location
+		const { id, title } = qs.parse(search.slice(1))
+		const res = messageDetails.find(detailsObj => {
+			return detailsObj.id === id
+		})
+		return (
+			<ul>
+				<li>ID：{id}</li>
+				<li>TITLE：{title}</li>
+				<li>CONTENT：{res.content}</li>
+			</ul>
+		)
+	}
+}
---
+++ 14_React脚手架/react_staging/17_向路由组件传递search参数/pages/Home/Message/index.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { Link, Route } from 'react-router-dom'
+
+import Details from './Details'
+
+export default class Message extends Component {
+	state = {
+		messageArr: [
+			{ id: '01', title: '消息1' },
+			{ id: '02', title: '消息2' },
+			{ id: '03', title: '消息3' }
+		]
+	}
+	render() {
+		const { messageArr } = this.state
+		return (
+			<div>
+				<ul>
+					{messageArr.map(msgObj => {
+						return (
+							<li key={msgObj.id}>
+								{/* 向路由组件传递 params 参数 */}
+								{/* <Link to={`/home/message/details/${msgObj.id}/${msgObj.title}`}>
+									{msgObj.title}
+								</Link> */}
+								{/* 向路由组件传递 search参数 */}
+								<Link
+									to={`/home/message/details/?id=${msgObj.id}&title=${msgObj.title}`}
+								>
+									{msgObj.title}
+								</Link>
+								&nbsp;&nbsp;
+							</li>
+						)
+					})}
+				</ul>
+				<hr />
+				{/* 声明接受 params 传参 */}
+				{/* <Route path="/home/message/details/:id/:title" component={Details} /> */}
+				{/* search 参数无需声明接受，正常注册路由即可 */}
+				<Route path="/home/message/details" component={Details} />
+			</div>
+		)
+	}
+}
