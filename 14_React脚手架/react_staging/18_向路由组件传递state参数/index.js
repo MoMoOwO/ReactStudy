2a05c6d7a35bdd1e5d5f8373babf72d80ca88161
MoMoOwO <strangersj@outlook.com>
2023-01-06 16:27:33 +0800

    向路由组件传递state参数

---
+++ 14_React脚手架/react_staging/18_向路由组件传递state参数/index.js
@@ -0,0 +1,11 @@
+// 引入 React 核心库
+import React from "react"
+// 引入 ReactDOM
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"
+// 引入 App 组件
+import App from './App'
+
+// 渲染 App 到页面
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<BrowserRouter><App /></BrowserRouter>)
